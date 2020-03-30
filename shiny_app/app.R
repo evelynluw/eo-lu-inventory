0361028c8f7dcbb69ee352d3ba66daaf692bd1c7
Maggie McDonough <maggiemcdonough@Maggies-MacBook-Pro-2.local>
2020-03-29 20:27:12 -0500

    fixed copy paste errors

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -6,24 +6,24 @@ library(tidyverse)
 library(readxl)
 library(googleway)
 library(shinydashboard)
-
+
 unlink("data", recursive=TRUE)
 dir.create("data")
-
+
 download.file("https://eo-lu-inventory.s3-us-west-1.amazonaws.com/for_shiny/eo_parcels/EO_Parcels.shp",'./data/eo_parcels.shp', method = "wget")
 download.file("https://eo-lu-inventory.s3-us-west-1.amazonaws.com/for_shiny/eo_parcels/EO_Parcels.dbf",'./data/eo_parcels.dbf', method = "wget")
 download.file("https://eo-lu-inventory.s3-us-west-1.amazonaws.com/for_shiny/eo_parcels/EO_Parcels.prj",'./data/eo_parcels.prj', method = "wget")
 download.file("https://eo-lu-inventory.s3-us-west-1.amazonaws.com/for_shiny/eo_parcels/EO_Parcels.shx",'./data/eo_parcels.shx', method = "wget")
-
+
 download.file("https://eo-lu-inventory.s3-us-west-1.amazonaws.com/for_shiny/assessor/assessor_ownership.csv",'./data/assessor.csv', method = "wget")
-
+
 eo_parcels_sf <- st_read("./data/eo_parcels.shp") %>%
   mutate(addr_full = sts_ddr) %>%
   rename(apn_sort = apn_srt)
-
+
 assessor <- read_csv("./data/assessor.csv") %>%
   mutate(addr_full = paste0(street_no," ",street_name," ",city," ",zip))
-
+
 server <- function(input, output, session) {
   
   output$map <- renderLeaflet({
@@ -57,8 +57,8 @@ server <- function(input, output, session) {
       fitBounds(bbox[1], bbox[2], bbox[3], bbox[4])
   })
 }
-
-
+
+
 # Define UI for application that draws a histogram
 ui <- dashboardPage(
   dashboardHeader(title = "East Oakland Land Use Explorer"),
@@ -89,5 +89,5 @@ ui <- dashboardPage(
     )
   )
 )
-
+
 shinyApp(ui = ui, server = server)
