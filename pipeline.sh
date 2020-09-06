3a663e8d6574678d769386df8c131fc9da54131f
Yuner Lu <yunerlu@berkeley.edu>
2020-09-06 11:12:59 -0700

    pipeline fix

--- pipeline.sh
+++ pipeline.sh
@@ -3,50 +3,50 @@
 # Author: Dan Sakaguchi and Evelyn Lu 
 # --------------------------------------
 
+echo "--------------- EXTRACTIONS -------------"
+
 # Setting up the environment
 echo "Updating pip"
 pip install -U pip
 echo "Activating virtual environment..."
 source .venv/bin/activate
 echo "Updating requirements..."
-pip install -r processors/requirements.txt
+pip install -r required_packages/requirements.txt
 
 # Remove existing data
-# echo "Cleaning up existing data folder..."
-# rm -rf data
-# echo "Re-making data folder..."
-# mkdir data
-# mkdir -p data/raw/
+echo "Cleaning up existing data folder..."
+rm -rf backend/data/scraping_out
+rm -rf backend/data/raw/
+
+echo "Re-making data folder..."
+mkdir -p backend/data/raw/
 
 # Extracting Data
-echo "--------------- EXTRACTIONS -------------"
-
 
 ## Run Scrapy spiders
 echo "------------------- Data Scraping -------------"
-# echo "Navigating to Scraper folder..."
-# cd processors/scraping/lui
+echo "Navigating to Scraper folder..."
+cd backend/processors/scraping/lui
 
-# echo "Scraping [ACCELA] Oakland Planning Permits (Zoning Clearances) Info"
-# scrapy crawl accela -o ../../../data/scraping_out/accela/accela_data.csv
-# echo "Finished scraping [ACCELA]"
+echo "Scraping [ACCELA] Oakland Planning Permits (Zoning Clearances) Info"
+scrapy crawl accela -o ../../../data/scraping_out/accela/accela_data.csv
+echo "Finished scraping [ACCELA]"
 
-# echo "Scraping [DCA] State Business Licenses (Records for Automotive Repair Dealers)"
-# scrapy crawl dca -o ../../../data/scraping_out/dca/dca_data.csv
-# echo "Finished scraping [DCA]"
+echo "Scraping [DCA] State Business Licenses (Records for Automotive Repair Dealers)"
+scrapy crawl dca -o ../../../data/scraping_out/dca/dca_data.csv
+echo "Finished scraping [DCA]"
 
-# echo "Scraping [HWTS] Hazardous Materials Transfer Data"
-# scrapy crawl hwts -o ../../../data/scraping_out/hwts/hwts_data.csv
-# echo "Finished scraping [HWTS]"
+echo "Scraping [HWTS] Hazardous Materials Transfer Data"
+scrapy crawl hwts -o ../../../data/scraping_out/hwts/hwts_data.csv
+echo "Finished scraping [HWTS]"
 
-# echo "Navigating back to top level..."
-# cd ../..
+echo "Navigating back to backend/..."
+cd ../../..
 
 echo "=================== Finished Data Scraping ==============="
 read -t 5 -n 1 -s -r -p "Press any key to continue and download Raw Data (cont. in 5 seconds)"
 
 ## Download Raw Data
-
 echo "------------------- Downloading Raw Data -------------"
 
 echo "Downloading Alameda County parcel polygons..."
@@ -96,7 +96,7 @@ python3 processors/Transformers.py convert_assessor_to_csv -f data/raw/assessor/
 echo "=================== Finished TRANSFORMATIONS ==============="
 
 # Data Cleaning
-### TODO: Process data cleaning here. Then output the cleaned data to data/shiny_in
+python3 backend/cleaning/clean.py
 
 # Upload Data
-### TODO: upload final data to S3 bucket
+python3 backend/pipeline/pipeline.py
